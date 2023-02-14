90574ec066c1879cc409cde6f06dd9e1ed0773c3
Pedro Cruz Avalos <106279254+PeterBoi@users.noreply.github.com>
2023-02-14 23:05:46 +0000

    changing out arrays to objects

--- script.js
+++ script.js
@@ -19,16 +19,58 @@ let displayLink = document.querySelector(".display-link");
 
 // task 7: create and populate an array to store your image urls. Create three more arrays. One to store your song names, one for the artists, and a last one for the song links.
 
-let images = ['https://i.ytimg.com/vi/hLlNJfBS3T4/maxresdefault.jpg', 'https://img.youtube.com/vi/AjGkbFqi67c/0.jpg', 'https://i1.sndcdn.com/artworks-000080331953-unkpcp-t500x500.jpg', 'https://upload.wikimedia.org/wikipedia/en/6/60/Album_Julieta_Venegas_limon_y_sal_cover.jpg', 'https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/a1/71/e1/a171e1e8-845e-b2ff-e748-52545f4f80ed/0.jpg/600x600bf-60.jpg'];
-let songNames = ['Mexico Lindo Y Querido', 'Lo Que Siento', 'La Chona', 'Limon Y Sal', 'Cielito Lindo'];
-let artists = ['Vicente Fernadez', 'Cuco', 'Los Tucanes De Tijuana', 'Julieta Venegas', 'Angela Aguilar'];
-let songLinks = ['https://www.youtube.com/watch?v=KfQzn081fes', 'https://www.youtube.com/watch?v=AjGkbFqi67c', 'https://www.youtube.com/watch?v=ULxcmMgsxf4', 'https://www.youtube.com/watch?v=IJ92gmhvZpM', 'https://www.youtube.com/watch?v=UP6Fc09GHQk'];
+// let images = ['https://i.ytimg.com/vi/hLlNJfBS3T4/maxresdefault.jpg', 'https://img.youtube.com/vi/AjGkbFqi67c/0.jpg', 'https://i1.sndcdn.com/artworks-000080331953-unkpcp-t500x500.jpg', 'https://upload.wikimedia.org/wikipedia/en/6/60/Album_Julieta_Venegas_limon_y_sal_cover.jpg', 'https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/a1/71/e1/a171e1e8-845e-b2ff-e748-52545f4f80ed/0.jpg/600x600bf-60.jpg'];
+// let songNames = ['Mexico Lindo Y Querido', 'Lo Que Siento', 'La Chona', 'Limon Y Sal', 'Cielito Lindo'];
+// let artists = ['Vicente Fernadez', 'Cuco', 'Los Tucanes De Tijuana', 'Julieta Venegas', 'Angela Aguilar'];
+// let songLinks = ['https://www.youtube.com/watch?v=KfQzn081fes', 'https://www.youtube.com/watch?v=AjGkbFqi67c', 'https://www.youtube.com/watch?v=ULxcmMgsxf4', 'https://www.youtube.com/watch?v=IJ92gmhvZpM', 'https://www.youtube.com/watch?v=UP6Fc09GHQk'];
 
 //REFACTOR ARRAYS DAY 
 // task 11: comment out the arrays data.
+
 // task 12: create an object for each of your songs.
+
+let Mexico_Lindo_Y_Querido;
+let Lo_Que_Siento;
+let La_Chona;
+let Limon_Y_Sal;
+let Cielito_Lindo;
+
 // task 13: inside each object, add key/value pairs to store the image url, song name, artist, and song link.
+
+Mexico_Lindo_Y_Querido = {
+  image: "https://i.ytimg.com/vi/hLlNJfBS3T4/maxresdefault.jp",
+  songName: "Mexico Lindo Y Querido",
+  artist: "Vicente Fernadez",
+  songLink: "https://www.youtube.com/watch?v=KfQzn081fes"
+}
+Lo_Que_Siento = {
+  image: "https://img.youtube.com/vi/AjGkbFqi67c/0.jpg",
+  songName: "Lo Que Siento",
+  artist: "Cuco",
+  songLink: "https://www.youtube.com/watch?v=AjGkbFqi67c"
+}
+La_Chona = {
+  image: "https://i1.sndcdn.com/artworks-000080331953-unkpcp-t500x500.jpg",
+  songName: "La Chona",
+  artist: "Los Tucanes De TiJuana",
+  songLink: "https://www.youtube.com/watch?v=ULxcmMgsxf4"
+}
+Limon_Y_Sal = {
+  image: "https://upload.wikimedia.org/wikipedia/en/6/60/Album_Julieta_Venegas_limon_y_sal_cover.jpg",
+  songName: "Limon Y Sal",
+  artist: "Julieta Venegas",
+  songLink: "https://www.youtube.com/watch?v=IJ92gmhvZpM"
+}
+Cielito_Lindo = {
+  image: "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/a1/71/e1/a171e1e8-845e-b2ff-e748-52545f4f80ed/0.jpg/600x600bf-60.jpg",
+  songName: "Cielito Lindo",
+  artist: "Angela Aguilar",
+  songLink: "https://www.youtube.com/watch?v=UP6Fc09GHQk"
+}
+
 // task 14: create an array that stores all of the objects.
+
+let songList = [Mexico_Lindo_Y_Querido, Lo_Que_Siento, La_Chona, Limon_Y_Sal, Cielito_Lindo];
 
 //REFACTOR LOOPS DAY 
 // task 15: update your `addSongInfo` function so the input values are saved in as values in a new object.
@@ -49,18 +91,18 @@ function addSongInfo() {
   let songLinkInput = songLink.value;
   console.log(songLinkInput);
   // task 10: use `.push()` to add each input value to the correct array.
-  images.push(imageInput);
-  console.log(images);
-  console.log(images.length);
-  songNames.push(songNameInput);
-  console.log(songNames);
-  console.log(songNames.length);
-  artists.push(artistInput);
-  console.log(artists);
-  console.log(artists.length);
-  songLinks.push(songLinkInput);
-  console.log(songLinks);
-  console.log(songLinks.length);
+  songList.push(imageInput);
+  console.log(songList);
+  console.log(songList.length);
+  songList.push(songNameInput);
+  console.log(songList);
+  console.log(songList.length);
+  songList.push(artistInput);
+  console.log(songList);
+  console.log(songList.length);
+  songList.push(songLinkInput);
+  console.log(songList);
+  console.log(songList.length);
 }
 
 /******** this function empties the display divs each time the button is clicked so that your playlist does not repeatedly add the data too many times. Where should this function be placed???********/
@@ -83,7 +125,7 @@ function displaySongInfo() {
   artists.forEach(function(singer) {
     displayArtist.insertAdjacentHTML('beforeend', `<p>${singer}</p>`)
   });
-  for(let i = 0; i < songLinks.length; i++){
+  for (let i = 0; i < songLinks.length; i++) {
     displayLink.insertAdjacentHTML('beforeend', `<a href='${songLinks[i]}'<p>Click here to listen</p></a>`)
   };
   // songLinks.forEach(function(url) {
